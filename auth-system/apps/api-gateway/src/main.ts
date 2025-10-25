d03e469cab141cfc2a7fc2b2be76d647b5e48385
kaderinho <adenihikader@gmail.com>
2025-10-25 15:49:25 +0200

    Setup API gateway with CORS, rate limiting, and proxy to auth service

--- auth-system/apps/api-gateway/src/main.ts
+++ auth-system/apps/api-gateway/src/main.ts
@@ -1,18 +1,44 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import proxy from "express-http-proxy";
+import rateLimit from "express-rate-limit";
 import * as path from "path";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
+// rate limit for abusif api request
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { err: "Too many request, Please try again later" },
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req: any) => req.ip,
+});
+
+app.use(limiter);
 
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 app.get("/api", (req, res) => {
   res.send({ message: "Welcome to api-gateway!" });
 });
+
+app.use("/", proxy("http://localhost:3000"));
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
--- auth-system/apps/auth-service/src/main.ts
+++ auth-system/apps/auth-service/src/main.ts
@@ -1,14 +1,26 @@
-import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
-});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+app.get("/", (req, res) => {
+  res.send({ message: "You gonna make it" });
+});
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Auth service is running at http://localhost:${port}/api`);
+});
+server.on("error", (err) => {
+  console.log("Server error:", err);
 });
